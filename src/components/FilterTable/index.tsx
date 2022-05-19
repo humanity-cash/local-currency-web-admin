d0279ca7bd74c99b526bf3ea97214112d08e4366
JeyDev310 <mtroz310@gmail.com>
2022-05-19 08:39:41 -0400

    make date filter

--- src/components/FilterTable/index.tsx
+++ src/components/FilterTable/index.tsx
@@ -10,8 +10,9 @@ import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
 import { fade } from '@material-ui/core/styles/colorManipulator';
 import { useHistory } from 'react-router-dom';
-import { IACHTransaction } from '../../types';
-
+import TableCell from '@material-ui/core/TableCell';
+import { Input, useFormControl } from '@material-ui/core';
+import { useState } from 'react';
 
 const useStyles = makeStyles({
 	tableStriped: {
@@ -72,6 +73,34 @@ const TransactionIdTypeProvider = (props: any) => (
 	<DataTypeProvider formatterComponent={TransactionIdFormatter} {...props} />
 );
 
+const UnitsFilterCell = ({ filter, onFilter }: any) => {
+	const [focused, setFocused] = useState(false);
+
+	return (
+		<TableCell >
+		<Input
+			style={{width: '100%'}}
+			type={focused ? 'date' : 'text'}
+			value={filter ? filter.value : ''}
+			onChange={e => onFilter(e.target.value ? { value: e.target.value } : null)}
+			placeholder="Filter..."
+			onFocus={() => setFocused(true)}
+			onBlur={() => setFocused(false)}
+		/>
+		</TableCell>
+	);
+
+}
+  
+  const FilterCell = (props: any) => {
+	const { column } = props;
+	if (column.name === 'createdAt' || column.name === 'confirmedAt') {
+	  return <UnitsFilterCell {...props} />;
+	}
+	return <TableFilterRow.Cell {...props} />;
+  };
+  
+  
 const FilterTable = ({columns, rows}: any) => {
 	const transactionIdColumn = ['transactionId'];
 	const usernameColumn = ['username'];
@@ -91,7 +120,9 @@ const FilterTable = ({columns, rows}: any) => {
 				<IntegratedPaging />
 				<Table tableComponent={TableComponentBase} />
 				<TableHeaderRow />
-				<TableFilterRow />
+				<TableFilterRow 
+					cellComponent={FilterCell}
+				/>
 				<PagingPanel />
 			</Grid>
 		</Paper>
